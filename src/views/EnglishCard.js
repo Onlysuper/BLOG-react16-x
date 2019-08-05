7452ef0e367a91af757bd535e1d3747897f62fa3
Onlysuper <2483613372@qq.com>
2019-08-05 18:05:00 +0800

    详情

--- src/views/EnglishCard.js
+++ src/views/EnglishCard.js
@@ -6,10 +6,8 @@ function AdminCard(props) {
       <div className='user-block-header'>{props.title}</div>
       <div className="user-box">
         <div className='content'>
-          <p>职业：女程序员，Web前端设计师</p>
-          <p>现居：四川省-成都市</p>
-          <p>工作室：青于蓝-排名技术建站</p>
-          <p>Email：dancesmiling@qq.com</p>
+          <p>No road of flowers leads to glory</p>
+          <p>没有一条通往荣誉的道路是铺满鲜花的</p>
         </div>
       </div>
     </div>
